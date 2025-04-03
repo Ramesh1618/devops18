2628e5f91bed6966912789dc70a339e0687c6232
Ramesh1618 <99476887+Ramesh1618@users.noreply.github.com>
2025-04-03 16:27:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "web_server_as" {
     image_id           = "ami-0a9a48ce4458e384e"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
-    key_name = "lastone"p
+    key_name = "usingpem.pem"
     tags = {
         Name = "DevOps"
     }
